e18178c8e0437414f9a41610c2a3c41e14ac3a78
legendhimslef <sabdulrahman971@gmail.com>
2023-07-30 11:37:39 +0200

    feat: complete the setup

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,257 @@ mod utils;
 use std::env;
 use std::process::Command;
 use tokio::time::Instant;
-use utils::{dir_size, elapsed_time, generate_filename, get_time_str, send_webhook_message};
+use utils::{
+    command_success, delete_files_if_more_than_3, dir_size, elapsed_time, find_oldest_file,
+    generate_filename, get_time_str, remove_id_index, send_webhook_message,
+};
 use webhook::client::WebhookClient;
 
 #[tokio::main]
 async fn main() {
     let instant = Instant::now();
+    let database_name = "Sofi-Test";
+    let destination_dir = "test/";
     let filename = generate_filename();
-    println!("Filename: {}", filename);
 
     let url = match env::var("BACKUP_WEBHOOK_URL") {
         Ok(value) => value.to_string(),
         Err(_) => panic!("BACKUP_WEBHOOK_URL must be set"),
     };
 
+    let mongodb_uri = match env::var("MONGODB_URI") {
+        Ok(value) => value.to_string(),
+        Err(_) => panic!("MONGODB_URI must be set"),
+    };
+
+    let ssh_origin = match env::var("SSH_ORIGIN") {
+        Ok(value) => value.to_string(),
+        Err(_) => panic!("SSH_ORIGIN must be set"),
+    };
+
+    let ssh_password = match env::var("SSH_PASSWORD") {
+        Ok(value) => value.to_string(),
+        Err(_) => panic!("SSH_PASSWORD must be set"),
+    };
+
     let client: WebhookClient = WebhookClient::new(url.as_str());
 
+    /****************
+     * Remove all the old mongo dump files
+     ****************/
     send_webhook_message(&client, "Cron job started, Removing all the files.").await;
     println!("{}: Cron job started.", get_time_str());
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("rm -rf /home/backup/{}", database_name))
+        .output()
+        .expect("failed to execute process");
+    let files_cmd = command_success(
+        &output,
+        format!("{}: Removing all the files.", get_time_str()).as_str(),
+    );
+    if !files_cmd {
+        send_webhook_message(&client, "Error removing all the files.").await;
+        println!("{}: Error removing all the files.", get_time_str());
+    } else {
+        send_webhook_message(&client, "All the files removed.").await;
+        println!("{}: All the files removed.", get_time_str());
+    }
 
-    let output = Command::new("sudo")
-        .args(&["rm -rf", "/home/test/Sofi/*"])
+    /****************
+     * Dump the mongo database
+     ****************/
+    send_webhook_message(&client, "Dumping the mongo database.").await;
+    let output = Command::new("/usr/bin/mongodump")
+        .arg(format!("--uri={}", mongodb_uri))
+        .arg(format!("-d={}", database_name))
+        .arg("-o=/home/backup/")
+        .output()
+        .expect("failed to execute process");
+    let dump_cmd = command_success(
+        &output,
+        format!("{}: Dumping the mongo database.", get_time_str()).as_str(),
+    );
+    if !dump_cmd {
+        send_webhook_message(&client, "Error dumping the mongo database.").await;
+        println!("{}: Error dumping the mongo database.", get_time_str());
+        panic!("Error dumping the mongo database.");
+    } else {
+        send_webhook_message(&client, "Mongo database dumped.").await;
+        println!("{}: Mongo database dumped.", get_time_str());
+    }
+
+    /****************
+     * Remove the _id_ index from the dump files
+     ****************/
+    send_webhook_message(&client, "Removing the _id_ index from the dump files.").await;
+    println!(
+        "{}: Removing the _id_ index from the dump files.",
+        get_time_str()
+    );
+    remove_id_index(database_name).await.unwrap();
+
+    /****************
+     * Zip the mongo dump files
+     ****************/
+    let dir_size = dir_size(format!("/home/backup/{}", database_name).as_str())
+        .await
+        .unwrap();
+
+    send_webhook_message(
+        &client,
+        format!("Zipping the mongo dump files. Directory size: {}", dir_size).as_str(),
+    )
+    .await;
+
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "/usr/bin/zip -9 /home/backup/zips/{} /home/backup/{}/*",
+            filename, database_name
+        ))
+        .output()
+        .expect("failed to execute process");
+    let zip_cmd = command_success(
+        &output,
+        format!("{}: Zipping the mongo dump files.", get_time_str()).as_str(),
+    );
+    if !zip_cmd {
+        send_webhook_message(&client, "Error zipping the mongo dump files.").await;
+        println!("{}: Error zipping the mongo dump files.", get_time_str());
+        panic!("Error zipping the mongo dump files.");
+    } else {
+        send_webhook_message(&client, "Mongo dump files zipped.").await;
+        println!("{}: Mongo dump files zipped.", get_time_str());
+    }
+
+    /****************
+     * delete the old file from the storage box
+     ****************/
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "sshpass -p '{}' ssh '-p23' {} 'ls ./test/'",
+            ssh_password, ssh_origin
+        ))
         .output()
         .expect("failed to execute process");
 
-    if let Ok(size) = dir_size("./").await {
-        println!("Total directory size: {} bytes", size);
+    let ls_cmd = command_success(
+        &output,
+        format!(
+            "{}: Finding the old file from the storage box.",
+            get_time_str()
+        )
+        .as_str(),
+    );
+
+    if !ls_cmd {
+        send_webhook_message(&client, "Error finding the old file from the storage box.").await;
+        println!(
+            "{}: Error finding the old file from the storage box.",
+            get_time_str()
+        );
+        panic!("Error finding the old file from the storage box.");
     } else {
-        eprintln!("Error calculating directory size");
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let old_file = find_oldest_file(stdout.as_ref());
+        if !old_file.is_empty() {
+            println!("Old file: {}", old_file);
+            send_webhook_message(
+                &client,
+                format!("Deleting oldest file `{}`", old_file).as_str(),
+            )
+            .await;
+
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg(format!(
+                    "sshpass -p '{}' ssh -p 23 {} 'rm ./test/{}'",
+                    ssh_password, ssh_origin, old_file
+                ))
+                .output()
+                .expect("failed to execute process");
+
+            let rm_cmd = command_success(
+                &output,
+                format!(
+                    "{}: Deleting the old file from the storage box.",
+                    get_time_str()
+                )
+                .as_str(),
+            );
+            if !rm_cmd {
+                send_webhook_message(&client, "Error deleting the old file from the storage box.")
+                    .await;
+                println!(
+                    "{}: Error deleting the old file from the storage box.",
+                    get_time_str()
+                );
+                panic!("Error deleting the old file from the storage box.");
+            } else {
+                send_webhook_message(&client, "Old file deleted.").await;
+                println!("{}: Old file deleted.", get_time_str());
+            }
+        }
     }
+
+    /****************
+     * Copy the zip file to the storage box
+     ****************/
+    send_webhook_message(&client, "Copying the zip file to the storage box.").await;
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "sshpass -p '{}' scp '-p23' /home/backup/zips/{} {}:{}",
+            ssh_password, filename, ssh_origin, destination_dir
+        ))
+        .output()
+        .expect("failed to execute process");
+
+    let scp_cmd = command_success(
+        &output,
+        format!(
+            "{}: Copying the zip file to the storage box.",
+            get_time_str()
+        )
+        .as_str(),
+    );
+
+    if !scp_cmd {
+        send_webhook_message(&client, "Error copying the zip file to the storage box.").await;
+        println!(
+            "{}: Error copying the zip file to the storage box.",
+            get_time_str()
+        );
+        panic!("Error copying the zip file to the storage box.");
+    } else {
+        send_webhook_message(&client, "Zip file copied.").await;
+        println!("{}: Zip file copied.", get_time_str());
+    }
+
+    /****************
+     * Delete the zip file from the server
+     ****************/
+    // delete only if zips folder has more than 3 files using rust read_dir function
+
+    send_webhook_message(&client, "Deleting the zip file from the server.").await;
+    println!("{}: Deleting the zip file from the server.", get_time_str());
+
+    delete_files_if_more_than_3("/home/backup/zips/")
+        .await
+        .unwrap();
+
+    /****************
+     * Cron job finished
+     ****************/
+
+    send_webhook_message(
+        &client,
+        format!("Cron job finished. Time Taken: {}", elapsed_time(instant)).as_str(),
+    )
+    .await;
+    println!("{}: Cron job finished.", get_time_str());
+
     println!("Time elapsed: {}", elapsed_time(instant));
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,7 @@
+use chrono::prelude::*;
 use chrono::Local;
 use serde::{Deserialize, Serialize};
-use tokio::fs;
+use tokio::fs::{read_dir, read_to_string, remove_file, write};
 use tokio::time::Instant;
 use webhook::client::WebhookClient;
 
@@ -9,43 +10,50 @@ pub fn get_time_str() -> String {
     now.format("%m/%d/%Y, %I:%M:%S %p").to_string()
 }
 
-pub async fn remove_id_index() -> Result<(), Box<dyn std::error::Error>> {
-    let dirname = "/home/backup/Sofi/";
-    let mut reader = fs::read_dir(dirname).await.unwrap();
-    loop {
-        if let Some(entry) = reader.next_entry().await? {
-            let filename = entry.file_name();
-            let filename = filename.to_str().unwrap();
-            let pat = format!("{}/{}", dirname, filename);
-            if filename.ends_with(".json") {
-                let content = fs::read_to_string(&pat).await?;
-                on_file_content(content, &pat).await?;
-            }
-        } else {
-            break;
+pub async fn remove_id_index(database_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let dirname = format!("/home/backup/{}/", database_name).to_string();
+    let mut reader = read_dir(dirname.clone()).await?; // Clone the dirname here
+    while let Some(entry) = reader.next_entry().await? {
+        let filename = entry.file_name();
+        let filename = filename.to_str().unwrap();
+        let pat = format!("{}/{}", dirname, filename);
+        if filename.ends_with(".json") {
+            let content = read_to_string(&pat).await?;
+            on_file_content(content, &pat).await?;
         }
     }
     Ok(())
 }
 
-pub async fn dir_size(directory: &str) -> Result<u64, Box<dyn std::error::Error>> {
+pub async fn dir_size(directory: &str) -> Result<String, Box<dyn std::error::Error>> {
     let mut total_size = 0;
-    let mut entries = fs::read_dir(directory).await?;
+    let mut entries = read_dir(directory).await?;
     while let Some(entry) = entries.next_entry().await? {
         let path = entry.path();
-        println!("{}", path.to_str().unwrap());
         if path.is_file() {
             let metadata = entry.metadata().await?;
             total_size += metadata.len();
         }
     }
-    Ok(total_size)
+
+    let size_in_mb = total_size as f64 / 1024.0 / 1024.0;
+    let size_in_gb = total_size as f64 / 1024.0 / 1024.0 / 1024.0;
+
+    let size_formatted = if size_in_gb >= 1.0 {
+        format!("{:.2} GB", size_in_gb)
+    } else {
+        format!("{:.2} MB", size_in_mb)
+    };
+
+    Ok(size_formatted)
 }
 
 pub async fn on_file_content(content: String, pat: &str) -> Result<(), Box<dyn std::error::Error>> {
     #[derive(Debug, Deserialize, Serialize)]
     struct Index {
         name: String,
+        key: serde_json::Value,
+        v: serde_json::Value,
     }
 
     #[derive(Debug, Deserialize, Serialize)]
@@ -54,9 +62,20 @@ pub async fn on_file_content(content: String, pat: &str) -> Result<(), Box<dyn s
     }
 
     let mut object: Object = serde_json::from_str(&content)?;
-    object.indexes.retain(|index| index.name != "_id_");
-    let updated_content = serde_json::to_string(&object)?;
-    fs::write(pat, updated_content).await?;
+    let mut new_indexes: Vec<Index> = Vec::new();
+
+    for index in object.indexes.iter() {
+        if index.name != "_id_" {
+            new_indexes.push(Index {
+                name: index.name.clone(),
+                key: index.key.clone(),
+                v: index.v.clone(),
+            })
+        }
+    }
+    object.indexes = new_indexes;
+    let new_content = serde_json::to_string(&object)?;
+    write(pat, new_content).await?;
     Ok(())
 }
 
@@ -89,4 +108,72 @@ pub async fn send_webhook_message(client: &WebhookClient, msg: &str) {
     if let Err(why) = fut {
         eprintln!("Error sending message: {:?}", why);
     }
+}
+
+pub fn command_success(output: &std::process::Output, msg: &str) -> bool {
+    if output.status.success() {
+        println!("{}", msg);
+        true
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        eprintln!("Error executing command: {}", stderr);
+        false
+    }
+}
+
+pub fn find_oldest_file(files_list: &str) -> &str {
+    let filenames: Vec<&str> = files_list
+        .split('\n')
+        .filter(|filename| !filename.is_empty())
+        .collect();
+
+    if filenames.len() < 6 {
+        return "";
+    }
+
+    let timestamps: Vec<i64> = filenames
+        .iter()
+        .map(|filename| {
+            let date_string = filename.split('_').next().unwrap();
+            let date = NaiveDate::parse_from_str(date_string, "%Y-%m-%d").unwrap();
+            date.and_hms_opt(0, 0, 0).unwrap().timestamp()
+        })
+        .collect();
+
+    let oldest_index = timestamps
+        .iter()
+        .position(|&ts| ts == *timestamps.iter().min().unwrap())
+        .unwrap();
+
+    filenames[oldest_index]
+}
+
+pub async fn delete_files_if_more_than_3(
+    directory: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut entries = read_dir(directory).await?;
+    let mut files: Vec<_> = Vec::new();
+
+    // Collect the files along with their last modified timestamp
+    while let Some(entry) = entries.next_entry().await? {
+        let path = entry.path();
+        if path.is_file() {
+            let metadata = entry.metadata().await?;
+            let last_modified = metadata.modified()?;
+            files.push((path, last_modified));
+        }
+    }
+
+    // Sort files by last modified timestamp in descending order
+    files.sort_by(|(_, t1), (_, t2)| t2.cmp(t1));
+
+    // Keep only the three latest files, delete the rest
+    for (i, (path, _)) in files.iter().enumerate() {
+        if i >= 3 {
+            remove_file(path).await?;
+            println!("Deleted file: {:?}", path);
+        }
+    }
+
+    Ok(())
 }
